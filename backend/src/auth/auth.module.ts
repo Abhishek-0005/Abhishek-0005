c2e51ca9dd6dbd8acfa02acfd111896b7e50c3e8
Abhishek-0005 <161461502+Abhishek-0005@users.noreply.github.com>
2026-07-07 13:09:13 +0530

    feat(backend): add AuthModule with Jwt/Passport and strategies

---
+++ backend/src/auth/auth.module.ts
@@ -0,0 +1,22 @@
+import { Module } from '@nestjs/common';
+import { PassportModule } from '@nestjs/passport';
+import { JwtModule } from '@nestjs/jwt';
+import { AuthService } from './auth.service';
+import { AuthController } from './auth.controller';
+import { UsersModule } from '../users/users.module';
+import jwtConfig from '../config/jwt.config';
+import { LocalStrategy } from './strategies/local.strategy';
+import { JwtStrategy } from './strategies/jwt.strategy';
+
+@Module({
+  imports: [
+    UsersModule,
+    PassportModule,
+    JwtModule.registerAsync({
+      useFactory: jwtConfig() as any,
+    }),
+  ],
+  controllers: [AuthController],
+  providers: [AuthService, LocalStrategy, JwtStrategy],
+})
+export class AuthModule {}
