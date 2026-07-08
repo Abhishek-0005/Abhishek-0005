86a2e454d82afeaf15d8acfa5200a81331464073
Abhishek-0005 <161461502+Abhishek-0005@users.noreply.github.com>
2026-07-08 16:18:40 +0530

    test(backend): add basic test for ExportController existence

---
+++ backend/src/modules/export/export.controller.spec.ts
@@ -0,0 +1,7 @@
+import { ExportController } from './export.controller';
+
+describe('ExportController', () => {
+  it('is defined', () => {
+    expect(new ExportController({} as any)).toBeTruthy();
+  });
+});
