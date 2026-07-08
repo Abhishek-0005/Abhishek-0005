38d00f4d34e1a6fffa6dc1c7a3d941714ef7c494
Abhishek-0005 <161461502+Abhishek-0005@users.noreply.github.com>
2026-07-08 16:36:50 +0530

    feat(backend): categories controller with endpoints

---
+++ expense-tracker/backend/src/modules/categories/categories.controller.ts
@@ -0,0 +1,29 @@
+import { Body, Controller, Delete, Get, Param, ParseIntPipe, Patch, Post } from '@nestjs/common';
+import { CategoriesService } from './categories.service';
+import { CreateCategoryDto } from './dto/create-category.dto';
+import { UpdateCategoryDto } from './dto/update-category.dto';
+
+@Controller('categories')
+export class CategoriesController {
+  constructor(private readonly service: CategoriesService) {}
+
+  @Get()
+  list() {
+    return this.service.list();
+  }
+
+  @Post()
+  create(@Body() dto: CreateCategoryDto) {
+    return this.service.create(dto);
+  }
+
+  @Patch(':id')
+  update(@Param('id', ParseIntPipe) id: number, @Body() dto: UpdateCategoryDto) {
+    return this.service.update(id, dto);
+  }
+
+  @Delete(':id')
+  delete(@Param('id', ParseIntPipe) id: number) {
+    return this.service.delete(id);
+  }
+}
