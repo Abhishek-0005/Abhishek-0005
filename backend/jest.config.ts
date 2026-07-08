a3a226aa7c8068d06afd78fe864484c8bcce709d
Abhishek-0005 <161461502+Abhishek-0005@users.noreply.github.com>
2026-07-08 11:09:23 +0530

    test: point ts-jest to tsconfig.jest.json

--- backend/jest.config.ts
+++ backend/jest.config.ts
@@ -7,9 +7,8 @@ const config: Config = {
   moduleFileExtensions: ['ts', 'js', 'json'],
   testMatch: ['**/*.spec.ts'],
   transform: {
-    '^.+\\.(t|j)s$': 'ts-jest',
+    '^.+\\.(t|j)s$': ['ts-jest', { tsconfig: '<rootDir>/tsconfig.jest.json' }],
   },
-  moduleNameMapper: {},
   collectCoverageFrom: ['src/**/*.ts', '!src/main.ts'],
 };
 
